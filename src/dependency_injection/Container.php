1707f872ca81685100f7c920520a3cd5aa75a4f7
RehArk <contact@vincent-clerc.net>
2024-09-24 08:42:39 +0200

    add dependency injection class

--- src/dependency_injection/Container.php
+++ src/dependency_injection/Container.php
@@ -2,60 +2,223 @@
 
 namespace Rehark\Carbon\dependency_injection;
 
+use ReflectionClass;
 use Rehark\Carbon\dependency_injection\exception\ClassOrInterfaceNotRegisterException;
-use Rehark\Carbon\dependency_injection\exception\UnknowTypeException;
 
-class Container {
+/**
+ * Class Container
+ *
+ * This class represents a Dependency Injection (DI) container that handles the
+ * registration and resolution of dependencies for classes and interfaces. 
+ * It follows the Singleton design pattern to ensure a single instance of the container.
+ */
+class Container
+{
 
+    /**
+     * @var Container Holds the single instance of the container (Singleton).
+     */
     private static Container $container;
 
-    public static function get() : Container {
+    /**
+     * Gets the single instance of the Container.
+     * If the instance does not exist, it creates one.
+     *
+     * @return Container The single instance of the container.
+     */
+    public static function get(): Container
+    {
 
-        if(!isset(self::$container)) {
+        if (!isset(self::$container)) {
             self::$container = new Container();
         }
 
         return self::$container;
-
     }
 
+    /**
+     * @var array Holds all the registered dependencies.
+     * Dependencies are stored in a multi-dimensional array categorized by 
+     * their types: class, interface, or instance.
+     */
     private array $dependencies = [];
 
+    /**
+     * Constructor
+     * 
+     * The constructor is private to enforce the Singleton pattern, preventing
+     * direct instantiation of the class.
+     */
     private function __construct() {}
 
-    public function register(string $key, $class) {
+    /**
+     * Registers a class, interface, or instance in the container.
+     *
+     * @param string $key The key representing the class, interface, or instance to register.
+     * @param mixed $class The actual class or instance to associate with the key.
+     */
+    public function register(string $key, $class)
+    {
         $type = $this->defineType($key);
         $this->dependencies[$type][$key] = $class;
     }
 
-    public function resolve(string $key) {
+    /**
+     * Resolves the given dependency by its key, instantiating the class or
+     * returning the instance if already register.
+     *
+     * @param string $key The key representing the dependency to resolve.
+     * @param array $parameters Optional parameters for class instantiation.
+     *
+     * @throws ClassOrInterfaceNotRegisterException If the dependency is not registered.
+     *
+     * @return mixed The resolved class instance or value.
+     */
+    public function resolve(string $key, array $parameters = [])
+    {
 
-        $type = $this->defineType($key);
-
-        if(!array_key_exists($key, $this->dependencies[$type])) {
+        if (!$this->dependencyExist($key)) {
             throw new ClassOrInterfaceNotRegisterException();
         }
 
-        return $this->build($this->dependencies[$type][$key]);
+        if ($this->instanceExist($key)) {
+            return $this->getValueOf($key);
+        }
 
+        return $this->build($this->getValueOf($key), $parameters);
     }
 
-    public function build($class) {
-        return new $class();
+    /**
+     * Builds and resolves the dependencies for a given class.
+     *
+     * This method uses reflection to inspect the constructor of the class and
+     * injects the required dependencies recursively.
+     *
+     * @param string $class The class to instantiate.
+     * @param array $parameters Optional parameters for dependency resolution.
+     *
+     * @return mixed A new instance of the class with dependencies injected.
+     */
+    public function build($class, $parameters)
+    {
+
+        $reflection = new ReflectionClass($class);
+        $constructor = $reflection->getConstructor();
+
+        if ($constructor === null) {
+            return new $class();
+        }
+
+        $dependencies = [];
+
+        foreach ($constructor->getParameters() as $key => $parameter) {
+
+            $parameterType = $parameter->getType();
+            $parameterName = $parameter->getName();
+
+            $dependencyName = $parameterType->getName();
+
+            $type = $this->defineType($parameterName);
+
+            // if parameter is in parameters, add as dependency
+            if (array_key_exists($dependencyName, $parameters)) {
+                $dependencies[] = $parameters[$dependencyName];
+                continue;
+            }
+
+            // if parameter is string, try to resolve by name
+            if ($dependencyName == 'string' && $type == 'instance') {
+                $dependencies[] = $this->resolve($parameterName);
+                continue;
+            }
+
+            // if parameter is optionnal, set default value
+            if ($parameter->isOptional()) {
+                $dependencies[] = $parameter->getDefaultValue();
+                continue;
+            }
+
+            // else try to resolve
+            $dependencies[] = $this->resolve($dependencyName);
+        }
+
+        return $reflection->newInstanceArgs($dependencies);
     }
 
-    private function defineType($key) : string {
+    /**
+     * Determines the type of the dependency based on the key.
+     * Returns 'class' if the key is a class, 'interface' if it's an interface,
+     * and 'instance' if it's a specific instance.
+     *
+     * @param string $key The key representing the dependency.
+     *
+     * @return string The type of the dependency ('class', 'interface', or 'instance').
+     */
+    private function defineType(string $key): string
+    {
 
-        if(class_exists($key)) {
+        if (class_exists($key)) {
             return 'class';
         }
 
-        if(interface_exists($key)) {
+        if (interface_exists($key)) {
             return 'interface';
         }
 
-        return throw new UnknowTypeException();;
-
+        return 'instance';
     }
 
+    /**
+     * Checks if a dependency is registered in the container.
+     *
+     * @param string $key The key representing the dependency.
+     *
+     * @return bool True if the dependency exists, false otherwise.
+     */
+    private function dependencyExist(string $key)
+    {
+        $type = $this->defineType($key);
+        return array_key_exists($key, $this->dependencies[$type]);
+    }
+
+    /**
+     * Checks if a given key represents an already instantiated instance.
+     *
+     * @param string $key The key representing the dependency.
+     *
+     * @return bool True if the dependency is an instance, false otherwise.
+     */
+    private function instanceExist(string $key)
+    {
+
+        if (!$this->dependencyExist($key)) {
+            return false;
+        }
+
+        if (!is_string($this->getValueOf($key))) {
+            return true;
+        }
+
+        if (
+            !class_exists($key) &&
+            !interface_exists($key)
+        ) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Retrieves the value of a registered dependency by its key.
+     *
+     * @param string $key The key representing the dependency.
+     *
+     * @return mixed The registered class, interface, or instance.
+     */
+    private function getValueOf(string $key)
+    {
+        $type = $this->defineType($key);
+        return $this->dependencies[$type][$key];
+    }
 }
--- test/dependency_injection/ContainerTest.php
+++ test/dependency_injection/ContainerTest.php
@@ -3,11 +3,17 @@
 use PHPUnit\Framework\TestCase;
 use Rehark\Carbon\dependency_injection\Container;
 use Rehark\Carbon\dependency_injection\exception\ClassOrInterfaceNotRegisterException;
-use Rehark\Carbon\dependency_injection\exception\UnknowTypeException;
+use Rehark\Carbon\http\method\HTTPMethods;
+use Rehark\Carbon\http\router\route\WebRoute;
+use Rehark\Carbon\http\router\Router;
+use Rehark\Carbon\http\router\uri\WebUri;
+use Rehark\Carbon\Test\utils\MethodsMocker;
 
-class ContainerTest extends TestCase {
+class ContainerTest extends TestCase
+{
 
-    public function testCreation() {
+    public function testCreation()
+    {
         $container = Container::get();
         $this->assertInstanceOf(Container::class, $container);
     }
@@ -26,17 +32,8 @@ class ContainerTest extends TestCase {
 
     }
 
-    public function testRegisterWithDependenciesFail() {
-    
-        $container = Container::get();
-
-        $this->expectException(UnknowTypeException::class);
-        $container->register('string', stdClass::class);
-        
-    }
-
     public function testRegisterWithInterfaceDependencies() {
-    
+
         $container = Container::get();
         $reflection = new ReflectionObject($container);
 
@@ -51,54 +48,131 @@ class ContainerTest extends TestCase {
         ];
 
         $this->assertEquals($expected, $dependencies);
-        
+
     }
 
-    public function testRegisterWithClassDependencies() {
-    
+    public function testRegisterWithClassDependencies()
+    {
+
         $container = Container::get();
         $reflection = new ReflectionObject($container);
 
         $container->register(stdClass::class, stdClass::class);
+        $container->register(DateTime::class, DateTime::class);
+        $container->register(WebUri::class, new WebUri('/'));
+        $container->register(WebRoute::class, WebRoute::class);
+        $container->register(Router::class, Router::class);
+        $container->register('route_path', __DIR__);
 
         $property = $reflection->getProperty('dependencies');
         $property->setAccessible(true);
         $dependencies = $property->getValue($container);
 
         $expected = [
-            'interface' => [Stringable::class => stdClass::class],
-            'class' => [stdClass::class => stdClass::class]
+            'interface' => [
+                Stringable::class => stdClass::class
+            ],
+            'instance' => [
+                'route_path' => __DIR__
+            ],
+            'class' => [
+                stdClass::class => stdClass::class,
+                DateTime::class => DateTime::class,
+                WebUri::class => new WebUri('/'),
+                WebRoute::class => WebRoute::class,
+                Router::class => Router::class
+            ]
         ];
 
         $this->assertEquals($expected, $dependencies);
-        
     }
 
     public function testResolveWithDependenciesFail() {
-    
+
         $container = Container::get();
 
         $this->expectException(ClassOrInterfaceNotRegisterException::class);
         $container->resolve(DateTimeInterface::class);
-        
+
     }
 
     public function testResolveWithInterfaceDependencies() {
-    
-        $container = Container::get();
-        $class = $container->resolve(Stringable::class);
 
+        $container = Container::get();
+
+        $class = $container->resolve(Stringable::class);
         $this->assertInstanceOf(stdClass::class, $class);
-        
+
     }
 
-    public function testResolveWithClassDependencies() {
-    
-        $container = Container::get();
-        $class = $container->resolve(stdClass::class);
+    public function testResolveWithNoParameter() {
 
+        $container = Container::get();
+
+        $class = $container->resolve(stdClass::class);
         $this->assertInstanceOf(stdClass::class, $class);
-        
+
+    }
+
+    public function testResolveWithParameters() {
+
+        $container = Container::get();
+
+        $date = $container->resolve(DateTime::class, ['string' => 'today']);
+
+        $webRoute = $container->resolve(WebRoute::class, [
+            WebUri::class => new WebUri('/'),
+            HTTPMethods::class => HTTPMethods::GET
+        ]);
+
+        $this->assertInstanceOf(DateTime::class, $date);
+        $this->assertEquals(new DateTime('today'), $date);
+
+        $this->assertInstanceOf(WebRoute::class, $webRoute);
+
+    }
+
+    public function testResolveWithStringDependency() {
+
+        $container = Container::get();
+
+        $class = $container->resolve(Router::class);
+        $this->assertInstanceOf(Router::class, $class);
+
+    }
+
+    public function testResolveWithOptionalParameter() {
+
+        $container = Container::get();
+
+        $class = $container->resolve(DateTime::class);
+
+        $this->assertInstanceOf(DateTime::class, $class);
+
+    }
+
+    public function testResolveWithRegisterDependencies() {
+
+        $container = Container::get();
+
+        $class = $container->resolve(WebRoute::class,[
+            HTTPMethods::class => HTTPMethods::GET
+        ]);
+
+        $this->assertInstanceOf(WebRoute::class, $class);
+
+    }
+
+    public function testInstanceExiste() {
+
+        $container = Container::get();
+        $instanceExist = MethodsMocker::getMethod(Container::class, 'instanceExist');
+
+        $container->register('date', new DateTime('today'));
+
+        $this->assertEquals($instanceExist->invokeArgs($container, ['notExistingKey']), false);
+        $this->assertEquals($instanceExist->invokeArgs($container, ['date']), true);
+
     }
 
 }
